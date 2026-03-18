611087ba439282a9a7fe05d210bf979e9c4d9b08
Mikael Alakari <mikael.alakari@rostek.fi>
2026-03-18 13:08:40 +0200

    url change

--- src/hooks/ApiHooks.ts
+++ src/hooks/ApiHooks.ts
@@ -4,7 +4,7 @@ import type { MetroArea } from '../interfaces/MetroArea';
 import type { Country } from '../interfaces/Country';
 
 const baseUrl =
-  'https://taxation-experts-consumers-collar.trycloudflare.com/api/v1';
+  'https://oaks-questionnaire-undertaken-college.trycloudflare.com';
 const getAuthToken = () => {
   try {
     const storedUser = JSON.parse(localStorage.getItem('user') || 'null');
