1ab90ebdf254869baaab83a9a69f579ace6432c1
Mikael Alakari <mikael.alakari@rostek.fi>
2026-03-23 14:56:32 +0200

    leaflet marker path

--- src/components/MapDiv.tsx
+++ src/components/MapDiv.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import L from 'leaflet';
 import { useProjects } from '../hooks/ApiHooks';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const defaultMarkerIcon = L.icon({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 const MapDiv: React.FC = () => {
   const { projects, getProjectsCoordinates } = useProjects();
@@ -24,6 +38,7 @@ const MapDiv: React.FC = () => {
           projects.map((project) => (
             <Marker
               key={project.id}
+              icon={defaultMarkerIcon}
               position={[
                 project.location?.latitude || 0,
                 project.location?.longitude || 0
